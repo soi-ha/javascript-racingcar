fed29c86a3f758f253373e29b4a70e1abae67777
soi-ha <soy2302ten@gmail.com>
2024-02-20 19:27:41 +0900

    test: eslint 테스트 코드 설정 추가 및 테스트 코드 구조 변경

--- __tests__/ScoreBoard.test.js
+++ __tests__/ScoreBoard.test.js
@@ -1,6 +1,6 @@
 import ScoreBoard from '../src/domain/models/ScoreBoard.js';
 
-const validInputsTest = () => {
+describe('스코어보드에 대한 유효성 테스트', () => {
   test('특수문자가 존재하지 않는 5글자 이하의 이름 리스트가 중복되지 않으면, 0으로 초기화된 Map 객체를 반환한다', () => {
     const validateInput = ['pobi', 'jun'];
 
@@ -9,17 +9,10 @@ const validInputsTest = () => {
     const scoreBoard = new ScoreBoard(validateInput);
     expect(scoreBoard.getScoreBoard()).toEqual(expectedResult);
   });
-};
 
-const invalidInputTest = () => {
   test.each([[['!df', 'soha']], [['brgndy', 'pobi']], [['왼손', '왼손', '공원']]])('이름 리스트 %s는 6글자 이상 혹은 특수문자 존재, 중복된 이름이 존재므로 에러를 발생시킨다.', (invalidInputs) => {
     expect(() => {
       const scoreBoard = new ScoreBoard(invalidInputs);
     }).toThrow('[ERROR]');
   });
-};
-
-describe('스코어보드에 대한 유효성 테스트', () => {
-  validInputsTest();
-  invalidInputTest();
 });
